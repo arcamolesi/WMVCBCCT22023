63b376ce6d57632e3b073480bd512474aee88c85
Almir Rogério Camolesi <camolesi@fema.edu.br>
2023-09-06 21:00:04 -0300

    Atualiza Create Labs

--- Controllers/LabsController.cs
+++ Controllers/LabsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections;
 using WMVCBCCT22023.Models;
 
 namespace WMVCBCCT22023.Controllers
@@ -7,7 +8,7 @@ namespace WMVCBCCT22023.Controllers
     public class LabsController : Controller
     {
         private readonly Contexto _context;
-
+      
 
         public LabsController(Contexto context)
         {
@@ -18,7 +19,33 @@ namespace WMVCBCCT22023.Controllers
         // GET: Salas
         public async Task<IActionResult> Index()
         {
+           
             return View(await _context.Salas.ToListAsync());
+        }
+
+        public IActionResult Banana()
+        {
+                return View();  
+        }
+
+       // [HttpGet]
+        public IActionResult Create()
+        {
+            return View();  
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,descricao,quantidade,situacao")] Sala sala)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sala);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sala);
         }
     }
 }
